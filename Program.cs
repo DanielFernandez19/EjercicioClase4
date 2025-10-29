724c145e9414e64487de6b04017ba895d057ba2c
DanielFernandez19 <reitmatch@hotmail.com>
2025-10-29 17:40:18 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿#region 1
+
+#endregion
+
+#region 2
+
+#endregion
+
+#region 3
+
+#endregion
+
+#region 4
+
+#endregion
+
+#region 5
+
+#endregion
+
+#region 6
+
+#endregion
+
+#region 7
+
+#endregion
+
+#region 8
+
+#endregion
+
+#region 9
+
+#endregion
+
+#region 10
+
+#endregion
